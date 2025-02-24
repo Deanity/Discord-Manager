6afc2e6e0bf22889c2b893bc6993e50841a6c02b
Dennaja <dendradetama2@gmail.com>
2025-02-24 17:01:48 +0800

    update perbaiki

---
+++ commands/moderation/Embed.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('embed')
+        .setDescription('Membuat embed dengan title dan deskripsi.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+    async execute(interaction) {
+
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+        const embed = new EmbedBuilder()
+            .setTitle(`<a:raja:1324772667877883977> Price List Jasa Farming CPS <a:raja:1324772667877883977>`)
+            .setDescription(`**<a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648> \n <:tangan:1343481170998067272>PTHT UWS ME 40 <:DL:1320541428274696292> \n <:tangan:1343481170998067272>PTHT UWS YOU 30 <:DL:1320541428274696292> \n <a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648> \n <:pukul:1320779662242353195> PNB CONSUME ME 10K/35 <:DL:1320541428274696292> \n <:pukul:1320779662242353195> PNB COMSUME YOU 10K/30 <:DL:1320541428274696292> \n <a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648> \n <:kotaksedot:1343482138917339177> FIND POG SEED 100K/13 <a:bgls:1324767458556514344> \n <:kotaksedot:1343482138917339177> FIND POG BLOCK 100K/240 <:DL:1320541428274696292> \n <a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648><a:rgb:1320721179606253648> \n - MIN ORDER 250+ UNDER? BEDA HARGA**`)
+            .setColor(`#${randomColor}`);
+
+        await interaction.deferReply({ ephemeral: true }); // Menghilangkan tampilan command reply
+        await interaction.deleteReply(); // Menghapus tampilan command setelah defer
+        await interaction.channel.send({ embeds: [embed] }); // Mengirim embed tanpa mereply
+    }
+};
+
+
+
+
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,25 +5,14 @@ module.exports = async (client) => {
     client.on('guildMemberAdd', async (member) => {
         const welcomeEmbed = new EmbedBuilder()
         .setColor('#00FF00')
-        .setTitle('🎉 Selamat Datang di Server! 🎉')
-        .setDescription(`Halo <@${member.id}>, selamat datang di **${member.guild.name}**! 🎊\n
-        Kami senang kamu bergabung! Jangan lupa untuk membaca peraturan server dan verify dirimu. 😊`)
-        .addFields(
-            { name: '👥 Total Member Sekarang', value: `**${member.guild.memberCount}**`, inline: true }
-        )
-        .setThumbnail(member.user.displayAvatarURL({ dynamic: true })) // Menampilkan avatar pengguna
-        .setImage('https://cdn.discordapp.com/attachments/1320314372123529236/1335444274149326848/standard.gif?ex=67a030e6&is=679edf66&hm=68ab4553c432c44e69c850a859496542a7bc9494be672dcafbe89d0ae1212e83&')
-        .setTimestamp()
-        .setFooter({ text: 'Welcome', iconURL: member.user.displayAvatarURL({ dynamic: true }) });
-    
+        .setDescription(`👋 Hai <@${member.id}>, selamat datang di **${member.guild.name}**!\n
+        Jangan lupa baca peraturan dan verify ya! 🎊`)
+        .setImage('https://cdn.discordapp.com/attachments/1320314372123529236/1335444274149326848/standard.gif?ex=67a030e6&is=679edf66&hm=68ab4553c432c44e69c850a859496542a7bc9494be672dcafbe89d0ae1212e83&');
+
         const channel = member.guild.channels.cache.get(welcomeChannelID);
         if (channel) {
-            // Kirim pesan mention + teks biasa
             await channel.send(`👋 Hai <@${member.id}>, selamat datang di **${member.guild.name}**! 🎉`);
-        
-            // Kirim embed welcome
             await channel.send({ embeds: [welcomeEmbed] });
         }
-    
     });
 };
